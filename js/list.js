30d1c2525d99024794ddab192514eeac47b14998
sem-utsc <156494699+sem-utsc@users.noreply.github.com>
2024-02-08 01:28:52 +0000

    Create the fetch implemtation using the rick and morty api

--- js/list.js
+++ js/list.js
@@ -1,16 +1,45 @@
 document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("#exampleModal").addEventListener('show.bs.modal', async event => {
+        // Button that triggered the modal
+        console.log(event);
+        var characterId = event.relatedTarget.getAttribute("data-detail");
+        const response = await fetch(`https://rickandmortyapi.com/api/character/${characterId}`);
+        const character = await response.json();
+        console.log(character);
+        var modalTemplate = document.getElementById("exampleModal")
+        modalTemplate.querySelector("[data-title]").textContent = character.name
+        modalTemplate.querySelector("[data-species]").textContent = character.species
+        modalTemplate.querySelector("[data-episode]").textContent = `Total Episodes: ${character.episode.length}`
+        var statusBadge = modalTemplate.querySelector("[data-status]")
+        statusBadge.textContent = character.status
+        statusBadge.setAttribute("data-status", character.status);
+        modalTemplate.querySelector("[data-img]").src = character.image;
+
+    });
     document.querySelector(".btn.btn-primary").addEventListener("click", event => {
-        var itemList = document.getElementById("my-list");
-        var template = document.getElementById("list-template");
-        var total = itemList.childElementCount + 1;
-        var clone = template.content.cloneNode(true);
-        clone.querySelector("[data-id='number']").textContent = `${total}`;
-        clone.querySelector("[data-id='title']").textContent = "Product";
-        clone.querySelector("[data-id='content']").textContent = "It's a new item";
-        itemList.appendChild(clone);
+        fetch("https://rickandmortyapi.com/api/character").then((response) => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            console.log(response);
+            return response.json();
+        }).then((response) => {
+            console.log(response);
+            var itemList = document.getElementById("my-list");
+            var template = document.getElementById("list-template");
+            response.results.forEach(element => {
+                var clone = template.content.cloneNode(true);
+                clone.querySelector(".col-xs-12").setAttribute("data-detail", element.id);
+                clone.querySelector("[data-id='title']").textContent = element.name;
+                clone.querySelector("[data-id='content']").textContent = element.species;
+                clone.querySelector("[data-id='img']").src = element.image;
+                itemList.appendChild(clone);
+            });
+        });
     });
     document.querySelector(".btn.btn-light").addEventListener("click", event => {
         var itemList = document.getElementById("my-list");
         itemList.replaceChildren();
     });
+    
 });
